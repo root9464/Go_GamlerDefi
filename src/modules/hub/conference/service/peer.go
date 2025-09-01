c02b8311c02b23f05a67944ea22fca8047142632
iO <senpai9464@yandex.ru>
2025-09-01 05:47:05 +0300

    fix: magnifique peer.go

--- src/modules/hub/conference/service/peer.go
+++ src/modules/hub/conference/service/peer.go
@@ -19,29 +19,18 @@ func (s *ConferenceService) SignalPeerConnections(requestID string, roomID strin
 		return
 	}
 
-	attemptSync := func() bool {
+	attemptSync := func() (tryAgain bool) {
 		room.Lock.Lock()
 		defer room.Lock.Unlock()
 
-		modified := false
 		for i := 0; i < len(room.Connections); i++ {
 			conn := room.Connections[i]
 
 			if conn.Pc.ConnectionState() == webrtc.PeerConnectionStateClosed {
 				room.Connections = append(room.Connections[:i], room.Connections[i+1:]...)
 				i--
-				modified = true
+				tryAgain = true
 				s.logger.Debugf("Connection removed because closed, uuid: %s, request_id: %s", conn.Kws.GetUUID(), requestID)
-				continue
-			}
-
-			if time.Since(conn.LastSignal) < conn.SignalDebounce {
-				s.logger.Debugf("Skipping connection due to debounce, uuid: %s, request_id: %s, last_signal: %v", conn.Kws.GetUUID(), requestID, conn.LastSignal)
-				continue
-			}
-
-			if conn.Pc.ConnectionState() == webrtc.PeerConnectionStateClosed {
-				s.logger.Debugf("Skipping connection because PeerConnection is closed, uuid: %s, request_id: %s", conn.Kws.GetUUID(), requestID)
 				continue
 			}
 
@@ -60,6 +49,7 @@ func (s *ConferenceService) SignalPeerConnections(requestID string, roomID strin
 				if _, ok := room.TrackLocals[sender.Track().ID()]; !ok {
 					if err := conn.Pc.RemoveTrack(sender); err != nil {
 						s.logger.Errorf("Failed to remove track, error: %v, uuid: %s, request_id: %s", err, conn.Kws.GetUUID(), requestID)
+						tryAgain = true
 					}
 				}
 			}
@@ -77,6 +67,7 @@ func (s *ConferenceService) SignalPeerConnections(requestID string, roomID strin
 						Direction: webrtc.RTPTransceiverDirectionSendonly,
 					}); err != nil {
 						s.logger.Errorf("Failed to add track, error: %v, uuid: %s, request_id: %s", err, conn.Kws.GetUUID(), requestID)
+						tryAgain = true
 					}
 				}
 			}
@@ -84,17 +75,20 @@ func (s *ConferenceService) SignalPeerConnections(requestID string, roomID strin
 			offer, err := conn.Pc.CreateOffer(nil)
 			if err != nil {
 				s.logger.Errorf("Failed to create offer, error: %v, uuid: %s, request_id: %s", err, conn.Kws.GetUUID(), requestID)
+				tryAgain = true
 				continue
 			}
 
 			if err = conn.Pc.SetLocalDescription(offer); err != nil {
 				s.logger.Errorf("Failed to set local description, error: %v, uuid: %s, request_id: %s", err, conn.Kws.GetUUID(), requestID)
+				tryAgain = true
 				continue
 			}
 
 			offerString, err := json.Marshal(offer)
 			if err != nil {
 				s.logger.Errorf("Failed to marshal offer, error: %v, uuid: %s, request_id: %s", err, conn.Kws.GetUUID(), requestID)
+				tryAgain = true
 				continue
 			}
 
@@ -103,51 +97,62 @@ func (s *ConferenceService) SignalPeerConnections(requestID string, roomID strin
 				Data:  string(offerString),
 			}); err != nil {
 				s.logger.Errorf("Failed to send offer, error: %v, uuid: %s, request_id: %s", err, conn.Kws.GetUUID(), requestID)
+				tryAgain = true
 				continue
 			}
 
-			conn.LastSignal = time.Now()
 			s.logger.Infof("Offer sent, uuid: %s, request_id: %s", conn.Kws.GetUUID(), requestID)
 		}
 
-		return modified
+		return tryAgain
 	}
 
-	for syncAttempt := 0; syncAttempt < 25; syncAttempt++ {
-		go func() {
-			time.Sleep(time.Second * 3)
-			if atomic.LoadUint32(&s.serverRunning) == 1 {
-				s.SignalPeerConnections(conference_utils.GenerateRequestID(), roomID)
-			}
-		}()
-
-		if !attemptSync() {
+	for syncAttempt := 0; ; syncAttempt++ {
+		if syncAttempt == 25 {
+			go func() {
+				time.Sleep(time.Second * 3)
+				if atomic.LoadUint32(&s.serverRunning) == 1 {
+					s.SignalPeerConnections(conference_utils.GenerateRequestID(), roomID)
+				}
+			}()
 			return
 		}
+
+		if !attemptSync() {
+			break
+		}
 	}
+
+	s.dispatchKeyFrame(roomID)
 }
 
 func (s *ConferenceService) StartKeyFrameDispatcher() {
-	ticker := time.NewTicker(time.Second * 5)
+	ticker := time.NewTicker(time.Second * 3)
 	defer ticker.Stop()
 	for range ticker.C {
 		if atomic.LoadUint32(&s.serverRunning) == 0 {
 			break
 		}
 		s.roomsLock.RLock()
-		for roomID, r := range s.rooms {
+		for roomID := range s.rooms {
 			s.dispatchKeyFrame(roomID)
-			s.logger.Infof("Dispatching key frame, room_id: %s, track_count: %d", roomID, r.TrackCount)
 		}
 		s.roomsLock.RUnlock()
 	}
 }
 
 func (s *ConferenceService) dispatchKeyFrame(roomID string) {
-	room := s.rooms[roomID]
+	s.roomsLock.RLock()
+	room, exists := s.rooms[roomID]
+	s.roomsLock.RUnlock()
+	if !exists {
+		return
+	}
 
 	room.Lock.RLock()
 	defer room.Lock.RUnlock()
+
+	s.logger.Infof("Dispatching key frame, room_id: %s, track_count: %d", roomID, len(room.TrackLocals))
 
 	for i := range room.Connections {
 		for _, receiver := range room.Connections[i].Pc.GetReceivers() {
